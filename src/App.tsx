bcf271eafe1ef7d5cea333f29ca5e789dcd9eda5
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2023-11-15 18:27:39 +0700

    build base

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import useRouteElements from './useRouteElements'
+
+function App() {
+  const routeElements = useRouteElements()
+  return <>{routeElements}</>
+}
+
+export default App
